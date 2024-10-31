f38fd984cdf77223646a346ee8abebe97e53be8b
Leonardo <leonardoa.dev@gmail.com>
2024-10-30 22:10:32 -0300

    Excluindo anexos da pergunta

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,9 +1,14 @@
 import type { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import type { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import type { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
+
+  constructor(
+    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+  ) {}
 
   async create(question: Question) {
     this.items.push(question)
@@ -48,6 +53,10 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   async delete(question: Question) {
     const questionIndex = this.items.findIndex(
       (item) => item.id === question.id,
+    )
+
+    this.questionAttachmentsRepository.deleteManyByQuestionId(
+      question.id.toString(),
     )
 
     this.items.splice(questionIndex, 1)
